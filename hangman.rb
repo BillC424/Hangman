792fbf6445b319df5a319218648a62838dc3cf8e
BillC424 <wcurran44@gmail.com>
2022-09-05 16:30:22 -0400

    Added @display_word instance variable using regex to show the amount of characters to HumanPlayer with _ characters replacing the letters in the secret word

--- hangman.rb
+++ hangman.rb
@@ -1,10 +1,11 @@
 class Hangman
-  attr_accessor :secret_word
+  attr_accessor :secret_word, :display_word
 
   def initialize
     @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    @remaining_rounds = 8
     @secret_word = 
-      game_words_array = []
+      game_words = []
       f = File.open('dictionary.txt', 'r')
       f.each do |word|
         word_length = word.length
@@ -12,13 +13,14 @@ class Hangman
         when 0..4, 13.. 
           nil
         else
-          game_words_array.push(word.chomp)
+          game_words.push(word.chomp)
         end
       end
-      @secret_word = game_words_array.sample
+      @secret_word = game_words.sample
+      @display_word = @secret_word.gsub(/[a-z]/, '_')
   end  
+  public
   
-
  # puts "You have #{} guesses left!"
  # puts 'You win!'
  # puts 'You lose!'
@@ -35,4 +37,4 @@ game = Hangman.new
 
 p game.secret_word
 
-
+p game.display_word
