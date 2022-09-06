541159708418f58de0cde15165348dc2afe91e23
BillC424 <wcurran44@gmail.com>
2022-09-06 00:07:36 -0400

    Renamed Hangman class as Computer and added a Game class to control flow of game. The game now is able to take human input for letter guesses, display correct guesses for the secret word, and end the game when the correct word is completed according to letter guesses

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,8 @@
-class Hangman
-  attr_accessor :secret_word, :display_word
+class Computer
+  attr_accessor :display_word 
+  attr_reader :game_won
 
   def initialize
-    @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    @remaining_rounds = 8
     @secret_word = 
       game_words = []
       f = File.open('dictionary.txt', 'r')
@@ -17,24 +16,67 @@ class Hangman
         end
       end
       @secret_word = game_words.sample
-      @display_word = @secret_word.gsub(/[a-z]/, '_')
+    @display_word = @secret_word.gsub(/[a-z]/, '_')
+    @game_won = ''
   end  
-  public
   
- # puts "You have #{} guesses left!"
- # puts 'You win!'
- # puts 'You lose!'
+  public
 
- # puts @alphabet.join(', ')
+  def check_word(letter_guess)
+    secret_word_array = @secret_word.split(//)
+    display_word_array = @display_word.split(//)
+    secret_word_array.each_with_index do |letter, index|
+      if letter == letter_guess
+        display_word_array[index] = secret_word_array[index]
+        @display_word = display_word_array.join
+      end
+    end
+  end
+
+  def check_if_game_over
+    if @display_word == @secret_word
+      @game_won = 'yes'
+      puts "You win! The secret word was #{@secret_word}. You get.... a brand new Hyundai Sonata! *plays Wheel of Fortune theme*"
+    end
+  end
+  
 end
 
 class HumanPlayer
+  attr_reader :alphabet  
+  def initialize
+    @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  end
 
+  public
+  def play_round
+    guess = gets.chomp
+    @alphabet.delete(guess)
+    guess.downcase
+  end  
 
 end
 
-game = Hangman.new
+class Game
 
-p game.secret_word
+def initialize
+@computer = Computer.new
+@human_player = HumanPlayer.new
+@remaining_rounds = 8
+self.play_game
+end
 
-p game.display_word
+def play_game
+  until @remaining_rounds == 0 || @computer.game_won == 'yes'
+    puts "Guess the word! #{@computer.display_word}. You have #{@remaining_rounds} guesses left."
+    puts "Choose from these letters #{@human_player.alphabet.join(', ')}"
+    @remaining_rounds = @remaining_rounds - 1
+    guess = @human_player.play_round
+    @computer.check_word(guess)
+    @computer.check_if_game_over
+  end  
+end
+
+end
+
+game = Game.new
