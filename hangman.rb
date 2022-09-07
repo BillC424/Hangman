2eb3343e90f110e5c50b80e2c5d3ad867671b163
BillC424 <wcurran44@gmail.com>
2022-09-07 14:38:30 -0400

    Added saved.yaml file to save game at any point during play. Still working on getting the load method to work properly

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,7 @@
+require 'yaml'
+
+#Class for generating secret word, updating word that displays to Human Player, and determining if guess is correct
+
 class Computer
   attr_accessor :display_word 
   attr_reader :game_won
@@ -31,6 +35,7 @@ class Computer
         @display_word = display_word_array.join
       end
     end
+    p @secret_word
   end
 
   def check_if_game_over(remaining_rounds)
@@ -44,6 +49,7 @@ class Computer
   
 end
 
+#Class for receiving input from human player for guesses
 class HumanPlayer
   attr_reader :alphabet  
   def initialize
@@ -74,6 +80,7 @@ class HumanPlayer
 
 end
 
+#Class for determining flow of game 
 class Game
 
 attr_reader :remaining_rounds
@@ -81,19 +88,43 @@ attr_reader :remaining_rounds
 def initialize
 @computer = Computer.new
 @human_player = HumanPlayer.new
-@remaining_rounds = 10
+@remaining_rounds = 5
 self.play_game
 end
 
 def play_game
   until @remaining_rounds == 0 || @computer.game_won == 'yes'
+    if @remaining_rounds == 5
+      puts 'Enter yes if you want to open a saved game'
+      if gets.chomp.downcase == 'yes'
+        self.load_game
+      end
+    end
+    puts 'Enter save if you want to save the game'
+    save = gets.chomp.downcase
+    if save == "save"
+        self.save_game
+    end
     puts "Guess the word by picking a letter! #{@computer.display_word}. You have #{@remaining_rounds} guesses left."
     puts "Choose from these letters #{@human_player.alphabet.join(', ')}"
     @remaining_rounds = @remaining_rounds - 1
     guess = @human_player.play_round
     @computer.check_word(guess)
     @computer.check_if_game_over(@remaining_rounds)
+    
   end  
+end
+
+def save_game
+  p yaml = YAML::dump(self)
+  p game_file = File.new("saved.yaml", "w")
+  p game_file.write(yaml)
+end
+
+def load_game
+  game_file = File.open("saved.yaml", "r")
+  yaml = game_file.read
+  YAML::load(yaml)
 end
 
 end
